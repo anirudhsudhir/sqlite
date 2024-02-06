18c62568ef931b83487baaa146409e7aa4512e8f
Anirudh Sudhir <137381184+anirudhsudhir@users.noreply.github.com>
2024-02-06 23:20:44 +0530

    feat: add query compiler and virtual machine

--- main.c
+++ main.c
@@ -6,11 +6,19 @@
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 
+typedef enum { METACOMMAND_SUCCESS, METACOMMAND_FAILURE } MetaCommandResult;
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+typedef enum { PREPARE_SUCCESS, PREPARE_FAILURE } PreparationResult;
+
 typedef struct {
   char *buffer;
   size_t buffer_length;
   ssize_t input_length;
 } InputBuffer;
+
+typedef struct {
+  StatementType type;
+} Statement;
 
 InputBuffer *new_input_buffer() {
   InputBuffer *input_buffer = malloc(sizeof(InputBuffer));
@@ -41,17 +49,62 @@ void close_input_buffer(InputBuffer *input_buffer) {
   free(input_buffer);
 }
 
+MetaCommandResult execute_meta_commands(InputBuffer *input_buffer) {
+  if (strcmp(input_buffer->buffer, ".exit") == 0) {
+    close_input_buffer(input_buffer);
+    exit(EXIT_SUCCESS);
+  } else {
+    return METACOMMAND_FAILURE;
+  }
+}
+
+PreparationResult prepare_statement(InputBuffer *input_buffer,
+                                    Statement *statement) {
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  } else if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+  return PREPARE_FAILURE;
+}
+
+void execute_prepared_statement(Statement *statement) {
+  switch (statement->type) {
+  case STATEMENT_INSERT:
+    printf("Performing an insert operation\n");
+    break;
+  case STATEMENT_SELECT:
+    printf("Performing a select operation\n");
+    break;
+  }
+}
+
 int main() {
   InputBuffer *input_buffer = new_input_buffer();
   while (true) {
     print_prompt();
     read_query(input_buffer);
 
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
-    } else {
-      printf("Command %s is unrecognized\n", input_buffer->buffer);
+    if (input_buffer->buffer[0] == '.') {
+      switch (execute_meta_commands(input_buffer)) {
+      case METACOMMAND_SUCCESS:
+        break;
+      case METACOMMAND_FAILURE:
+        printf("Invalid meta command '%s'\n", input_buffer->buffer);
+        continue;
+      }
+    }
+
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement)) {
+    case PREPARE_SUCCESS:
+      execute_prepared_statement(&statement);
+      break;
+    case PREPARE_FAILURE:
+      printf("Unable to process SQL command '%s'\n", input_buffer->buffer);
+      break;
     }
   }
 }
